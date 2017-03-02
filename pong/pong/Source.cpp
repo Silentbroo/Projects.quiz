2e9623bcf8d07b7008058106e737ad17b8b0dc6f
Game110AM <Game110AM@605-110-STU21>
2017-03-02 09:26:22 -0700

    pong

--- pong/pong/Source.cpp
+++ pong/pong/Source.cpp
@@ -60,11 +60,11 @@ int main()
 	/////
 	display = al_create_display(640, 480);
 	////////ball
-	ball = al_create_bitmap(10, 10);
+	ball = al_create_bitmap(25, 25);
 
 	al_set_target_bitmap(ball);
 
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_clear_to_color(al_map_rgb(150, 0, 150));
 	////
 	padle = al_create_bitmap(150, 32);
 
@@ -110,31 +110,45 @@ int main()
 			//if the left button is pressed AND we're still right of the left wall
 			//move the box left by 4 pixels
 			if (key[0] && padle_x >= 0) {
-				padle_x -= 4.0;
+				padle_x -= 5.0;
 			}
 
 			//if the right button is pressed AND we're still left of the right wall
 			//move the box right by 4 pixels
 			if (key[1] && padle_x <= 640 - 32) {
-				padle_x += 4.0;
+				padle_x += 5.0;
+			}
+
+
+
+			if (ball_x < 0 || ball_x > 640 - 32) {
+				//flip the x direction
+				ball_dx = -ball_dx;
 			}
 
 			//redraw at every tick of the timer
 			redraw = true;
-		}
-		//WSAD Controles
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
+
 
 
 			if (key2[0] && padle2_x >= 0) {
-				padle2_x -= 4.0;
+				padle2_x -= 5.0;
 			}
 
 
 			if (key2[1] && padle2_x <= 640 - 32) {
-				padle2_x += 4.0;
+				padle2_x += 5.0;
+			}
+			//if the box hits the top wall OR the bottom wall
+			if (ball_y < 0 || ball_y > 480 - 32) {
+				//flip the y direction
+				ball_dy = -ball_dy;
 			}
 
+			//really important code!
+			//move the box in a diagonal
+			ball_x += ball_dx;
+			ball_y += ball_dy;
 
 			redraw = true;
 		}
@@ -177,8 +191,8 @@ int main()
 				key2[1] = true;
 				break;
 			}
-
-			//has something been released on the keyboard?
+		}
+		//has something been released on the keyboard?
 		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
 
@@ -208,80 +222,48 @@ int main()
 
 			}
 
-		
-		}
-		ALLEGRO_EVENT ev;
-		//this function will wait forever if no events come in
-		al_wait_for_event(event_queue, &ev);
-
-		//////////////////////////////////////////////////////////////////////////////////
-		//this section handles the bouncing-off-the-edges action
-
-		//sets this part of the game to run on the clicks of the timer 
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
-			//if the box hits the left wall OR the right wall
-			if (ball_x < 0 || ball_x > 640 - 32) {
-				//flip the x direction
-				ball_dx = -ball_dx;
-			}
-			//if the box hits the top wall OR the bottom wall
-			if (ball_y < 0 || ball_y > 480 - 32) {
-				//flip the y direction
-				ball_dy = -ball_dy;
-			}
-
-			//really important code!
-			//move the box in a diagonal
-			ball_x += ball_dx;
-			ball_y += ball_dy;
-
-			//if an event happened, you better redraw
-			redraw = true;
 		}
 
-		//////////////////////////////////////////////////////////////////
-		//kill program if the user clicks the exit button
-		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-			break;
-		}
 
-		//if you were supposed to redraw but there wasn't an event, don't redraw
+
+
+
+
+
+
+
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		//if the clock ticked but no other events happened, don't bother redrawing
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 
+			//paint black over the old screen, so the old square dissapears
 			al_clear_to_color(al_map_rgb(0, 0, 0));
-			//if the clock ticked but no other events happened, don't bother redrawing
-			if (redraw && al_is_event_queue_empty(event_queue)) {
-				redraw = false;
 
-				//paint black over the old screen, so the old square dissapears
-				al_clear_to_color(al_map_rgb(0, 0, 0));
+			//the algorithm above just changes the x and y coordinates
+			//here's where the bitmap is actually drawn somewhere else
+			al_draw_bitmap(padle, padle_x, padle_y, 0);
 
-				//the algorithm above just changes the x and y coordinates
-				//here's where the bitmap is actually drawn somewhere else
-				al_draw_bitmap(padle, padle_x, padle_y, 0);
+			al_draw_bitmap(padle2, padle2_x, padle2_y, 0);
 
-				al_draw_bitmap(padle2, padle2_x, padle2_y, 0);
-
-				al_draw_bitmap(ball, ball_x, ball_y, 0);
+			al_draw_bitmap(ball, ball_x, ball_y, 0);
 
 
 
-				al_flip_display();
-
-			}
-		}//end game loop
-
-		al_destroy_bitmap(padle);
-		al_destroy_bitmap(padle2);
-		al_destroy_bitmap(ball);
-		al_destroy_timer(timer);
-		al_destroy_display(display);
-		al_destroy_event_queue(event_queue);
-
-		return 0;
-
+			al_flip_display();
 
 		}
-	}
-	
+	}//end game loop
+
+	al_destroy_bitmap(padle);
+	al_destroy_bitmap(padle2);
+	al_destroy_bitmap(ball);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+
+	return 0;
+
+
+
+}
